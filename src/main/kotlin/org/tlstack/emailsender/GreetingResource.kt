1ea93489c7b2df8637e8b5a3556ae8ef59b62df7
Caio <caiow.wk@gmail.com>
2022-01-27 00:08:14 -0300

    feat: add initial kotlin quarkus setup

---
+++ src/main/kotlin/org/tlstack/emailsender/GreetingResource.kt
@@ -0,0 +1,14 @@
+package org.tlstack.emailsender
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/greeting")
+class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "Hello RESTEasy Reactive"
+}
